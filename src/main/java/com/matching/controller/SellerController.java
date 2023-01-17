cf5029d5f3e1d945335e9012445c1db0d63c6a0b
DRIPMASTER <qsccsq4444@naver.com>
2023-01-17 15:28:35 +0900

    tset

--- src/main/java/com/matching/controller/SellerController.java
+++ src/main/java/com/matching/controller/SellerController.java
@@ -8,17 +8,4 @@ import org.springframework.web.bind.annotation.RestController;
 @RequiredArgsConstructor
 public class SellerController {
 
-    // 프로필 설정 put /seller
-
-    // 프로필 조회 get /seller
-
-    // 판매 상품 등록 post /seller/products
-
-    // 판매 상품 조회 get /seller/products
-
-    // 판매 상품 수정 put /seller/updateproduct/{id}
-
-    // 판매 상품 삭제 put /seller/deleteproducts/{id}
-
-    // 고객 요청 목록 조회 get /seller/clientList
 }
